38531595641467cc36da4f4ea7fbefd9d470387f
Donatella Tesei <donatella.tesei@gmail.com>
2022-11-09 14:35:47 +0100

    exercise 3

---
+++ Exercise3_lv.php
@@ -0,0 +1,38 @@
+<?php
+
+$numeric_array = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+function funny($arrayMaxNum)
+{
+    $maxNumb = max($arrayMaxNum);
+    return $maxNumb;
+}
+$return_value = funny($numeric_array);
+echo "The highest value from the function is: $return_value";
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print my name</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    ?>
+
+</body>
+
+
+</html>
