dbeef0d7296fda0aca2708a1e6a31eaddfc8ea6a
Donatella Tesei <donatella.tesei@gmail.com>
2022-11-09 13:29:27 +0100

    Exercise 1

---
+++ Exercise1.php
@@ -0,0 +1,54 @@
+<?php
+/*for loop*/
+for ($i = 0; $i < 50; $i++) {
+    $k = $i + 1;
+
+    echo "Donatella" . $k . "<br>";
+}
+
+?>
+<br>
+
+<?php
+/*while loop*/
+$i = 0;
+while ($i < 50) {
+    $i++;
+    echo "Donatella" . "<br>";
+}
+?>
+<br>
+
+<?php
+/*do while loop*/
+$i = 0;
+do {
+    $i++;
+    echo "Donatella" . "<br>";
+} while ($i < 50);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print my name</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    ?>
+
+</body>
+
+
+</html>
