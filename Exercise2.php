1c16844832c68f17ac4eda99384baf764f40ae91
Donatella Tesei <donatella.tesei@gmail.com>
2022-11-09 13:29:42 +0100

    Exercise2

---
+++ Exercise2.php
@@ -0,0 +1,33 @@
+<?php
+
+//array 
+$numeric_array = array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+foreach ($numeric_array as $value) {
+    echo "Value is $value <br>";
+}
+
+?>
+<br>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Print my name</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    ?>
+
+</body>
+
+
+</html>
